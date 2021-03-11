ddfb2e28fa1b738b954e1ef40e64413700dd7ab2
Boyuan Zhang <bzhang.dev@gmail.com>
2021-03-11 15:18:24 +0000

    deploy Thu Mar 11 2021 15:18:24 GMT+0000 (Coordinated Universal Time)

---
+++ dist/index.js
@@ -0,0 +1,11 @@
+/*#__PURE__*/
+React.createElement("template", null, /*#__PURE__*/React.createElement("lightning-card", {
+  title: "HelloWorld",
+  "icon-name": "custom:custom14"
+}, /*#__PURE__*/React.createElement("div", {
+  class: "slds-m-around_medium"
+}, /*#__PURE__*/React.createElement("p", null, "Hello, ", greeting, "!"), /*#__PURE__*/React.createElement("lightning-input", {
+  label: "Name",
+  value: greeting,
+  onchange: changeHandler
+}))));
